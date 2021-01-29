99256e7f8419648a4ea744ef25a16f6e0ee79dce
Jblanch94 <johnnyblanchard94@gmail.com>
2021-01-29 15:35:18 -0500

    Added in a app file for starting the server with some configuration

--- server/build/index.js
+++ server/build/index.js
@@ -1,17 +1,5 @@
 "use strict";
-var __importDefault = (this && this.__importDefault) || function (mod) {
-    return (mod && mod.__esModule) ? mod : { "default": mod };
-};
 Object.defineProperty(exports, "__esModule", { value: true });
-var express_1 = __importDefault(require("express"));
-var DatabaseService_1 = require("./services/DatabaseService");
-var models_associations_1 = require("./utils/models.associations");
-var app = express_1.default();
-var PORT = process.env.PORT || 5000;
-var db = DatabaseService_1.sequelize.getInstance;
-var associations = new models_associations_1.ModelAssociations(db);
-associations.setupRelations();
-db.sync({ force: true });
-app.listen(PORT, function () {
-    console.log("Listening on port " + PORT);
-});
+var App_1 = require("./App");
+var app = new App_1.App();
+app.start();
